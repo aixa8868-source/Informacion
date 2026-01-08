117c083c617ba99dba6ef3e5fd0f01cd9ac0dbd0
aixa8868-source <aixa8868@gmail.com>
2026-01-08 11:36:50 -0600

    Add files via upload

---
+++ azul.py
@@ -0,0 +1,33 @@
+print("Nombre completo:")
+print("Mildred Azul Cardenas López")
+
+print("Fortalezas:")
+print("- Responsable")
+print("- Creativa")
+print("- Perseverante")
+
+print("Debilidades:")
+print("- A veces me enojo por cosas imsignificadamente")
+
+print("Gustos:")
+print("- Escuchar música")
+print("- Dormir")
+print("- Andar en bicicleta")
+print("- Nadar")
+
+print("Canciones favoritas:")
+print("- 'Si no quieres no")
+print("- 'Presidente'")
+
+print("Artistas favoritos:")
+print("- Carin León")
+print("- Luis R Conriquez")
+
+print("Resumen de mis vacaciones:")
+print("Durante mis vacaciones descansé en casa, pasando tiempo con mi familia,")
+print("salí con mis primos, limpie la casa y escuché música y aproveché para relajarme.")
+print("También realicé algunas actividades pendientes en casa.")
+print()
+
+print("Estado de ánimo actual:")
+print("Motivada y entusiasmada ")
