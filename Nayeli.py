6699110695b880ac3da83e7d17b2afa2cba32138
nayeli-ship-it <uzz.peron@gmail.com>
2026-01-08 12:05:04 -0600

    Create Nayeli.py

---
+++ Nayeli.py
@@ -0,0 +1,28 @@
+# Información personal de Nayeli
+
+nombre = "Nayeli Alexandra Perón Nava"
+fortalezas = "Mis hermanas"
+debilidades = "Enojarme mucho"
+gustos = "Escuchar música"
+cancion_favorita = "Agosto"
+artista_favorito = "Bad Bunny"
+artista_extra = "Olivia Rodrigo"
+vacaciones = "Salí con mi familia"
+
+print("====== INFORMACIÓN PERSONAL ======")
+print("Nombre:", nombre)
+print()
+print("Fortalezas:", fortalezas)
+print("Debilidades:", debilidades)
+print("Gustos:", gustos)
+print()
+print("Canción favorita:", cancion_favorita)
+print("Artista de la canción:", artista_favorito)
+print("Artista favorito:", artista_extra)
+print()
+print("¿Qué hice en vacaciones?")
+print(vacaciones)
+print()
+print("Conclusión:")
+print("Este programa muestra la información personal de Nayeli.")
+print("Sirve para conocer mejor sus gustos, fortalezas y experiencias.")
