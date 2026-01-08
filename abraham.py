6d18c2c834b5c2f4ee3d26d1d0461d3a560ed718
brajam1234 <brajamjimenez@gmail.com>
2026-01-08 11:48:47 -0600

    Add files via upload

---
+++ abraham.py
@@ -0,0 +1,30 @@
+print("Nombre completo: Abraham Munoz Hernandez")
+
+print("Fortalezas:")
+print("- Responsable")
+print("- Perseverante")
+print("- Aprendo rápido")
+
+print("Debilidades:")
+print("- A veces me distraigo")
+print("- Soy un poco impaciente")
+
+print("Gustos:")
+print("- Escuchar música")
+print("- Usar la computadora")
+print("- Pasar tiempo con personas importantes")
+
+print("Canciones favoritas:")
+print("- Sin tu amor")
+print("- salud y vida")
+print("- Yellow")
+
+print("Artistas favoritos:")
+print("- Ozuna")
+
+print("Resumen de vacaciones:")
+print("Durante mis vacaciones descansé, pasé tiempo con mi familia,")
+print("escuché música, usé el celular y conviví con personas cercanas en estas fechas navidenas.")
+
+print("Estado de ánimo:")
+print("Tranquilo")
