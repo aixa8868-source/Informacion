a8c0b82a566b63a864d795015921c1bafec52b78
DanyJordan <danielajordandaniela@gmail.com>
2026-01-08 11:22:19 -0600

    Add files via upload

---
+++ daniela.py
@@ -0,0 +1,33 @@
+print("Nombre completo:")
+print("Daniela Jordan Barrera")
+
+print("Fortalezas:")
+print("- Responsable")
+print("- Creativa")
+print("- Perseverante")
+
+print("Debilidades:")
+print("- A veces me distraigo fácilmente Me pongo nerviosa al hablar en público")
+
+print("Gustos:")
+print("- Escuchar música")
+print("- Ver series")
+print("- Pasar tiempo con mi familia")
+print("- Aprender cosas nuevas en tecnología")
+
+print("Canciones favoritas:")
+print("- 'Lo tienes todo ")
+print("- 'Ojitos Lindos'")
+
+print("Artistas favoritos:")
+print("- Eden Muñoz")
+print("-Julion Álvarez")
+
+print("Resumen de mis vacaciones:")
+print("Durante mis vacaciones descansé en casa, pasé tiempo con mi familia,")
+print("salí con amigos, escuché música y aproveché para relajarme.")
+print("También realicé algunas actividades escolares pendientes.")
+print()
+
+print("Estado de ánimo actual:")
+print("Motivada ")
